82e2d782004afc2a0fa43b4e565aa930a82f9696
Dan Thompson <dthompsonpython@gmail.com>
2019-10-01 14:01:49 -0400

    Added input of name to test

--- senpy.py
+++ senpy.py
@@ -1,10 +1,17 @@
+import argparse
 import shutil
 import os
 import pytest
 
 
+parser = argparse.ArgumentParser(description='Senpy noticed you.')
+parser.add_argument('testname', metavar='name', type=str,
+                    help='test name')
+
+args = parser.parse_args()
+
 try:
-    pytest.main(["-x", "tests/examples"])
+    pytest.main(["-x", "tests/{}".format(args.testname)])
 except():
     pass
 shutil.rmtree("dojo")
